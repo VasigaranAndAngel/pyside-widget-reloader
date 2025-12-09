ed992c7a8160645282e1f65a6e73f3d4a3d113a2
VasigaranAndAngel <72515046+VasigaranAndAngel@users.noreply.github.com>
2025-12-09 13:27:24 +0530

    rename package to "pwreloader" and restructure project dir structure

--- pyside_widget_reloader/__init__.py
+++
@@ -1,4 +0,0 @@
-from .src.main import start_reloaders
-from .src.window import ReloaderWindow
-
-__all__ = ["start_reloaders", "ReloaderWindow"]
---
+++ src/pwreloader/__init__.py
@@ -0,0 +1,4 @@
+from .main import start_reloaders
+from .window import ReloaderWindow
+
+__all__ = ["start_reloaders", "ReloaderWindow"]
